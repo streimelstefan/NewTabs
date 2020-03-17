8aa40476fee79031da50d2e85f3f7af9d3b87e6e
Stefan Streimel <stefan@streimel.com>
2020-03-17 16:18:46 +0100

    added command list

--- js/config.js
+++ js/config.js
@@ -4,5 +4,27 @@ const config = {
             name: 'Google',
             searchPrefix: 'https://www.google.com/search?q='
         }
+    ],
+    commands: [
+        {
+            key: 'g',
+            action: googleAction
+        },
+        {
+            key: 'y',
+            action: yahooAction
+        },
+        {
+            key: 'd',
+            action: duckduckgoAction
+        },
+        {
+            key: 'b',
+            action: bingAction
+        },
+        {
+            key: 'standart',
+            action: standardAction
+        }
     ]
 }
--- js/searchlogic.js
+++ js/searchlogic.js
@@ -3,9 +3,15 @@ const input = document.getElementById('searchText');
 document.getElementById('searchForm').addEventListener('submit', (event) => {
     event.preventDefault();
 
-    const query = config.searchproviders[0].searchPrefix + input.value;
+    if (input.value.startsWith(':')) {
 
-    console.log(query);
+    } else {
 
-    window.location.href = query;
+        
+        const query = config.searchproviders[0].searchPrefix + input.value;
+        
+        console.log(query);
+        
+        window.location.href = query;
+    }
 })
