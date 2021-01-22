0be255d9c1cb234738927900a524680003428c28
Stefan Streimel <stefan@streimel.com>
2021-01-22 20:55:20 +0100

    Created function the load an Image and convert it into an usable URL

--- chrome_extension/site/src/routes/_utils/background.ts
+++ chrome_extension/site/src/routes/_utils/background.ts
@@ -20,12 +20,24 @@ export async function loadImage(url: string) {
     
 }
 
-export async function loadImgFromUrl(url: string): Promise<any> {
-
-}
-
-export async function encodeImgToString(img): Promise<string> {
-    return "";
+/**
+ * Loads Image from an URL and returns the string as a base64 encoded string
+ * @param url URL to load the Image from
+ */
+export async function loadImgFromUrl(url: string): Promise<String> {
+    return new Promise<any>((res, rej) => {
+        const xhr = new XMLHttpRequest();
+        xhr.onload = () => {
+            const reader = new FileReader();
+            reader.onloadend = () => {
+                res(reader.result);
+            }
+            reader.readAsDataURL(xhr.response);
+        };
+        xhr.open('GET', url);
+        xhr.responseType = 'blob';
+        xhr.send();
+    });
 }
 
 export async function saveImage(imgString: string) {
